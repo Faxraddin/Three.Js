58d959724568522f2a731b6d140e9bf9a2912167
Faxraddin <reydivonay@gmail.com>
2023-12-27 20:00:11 +0400

    starting a new course,old one did not work out

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import './App.css';
 import Main from './pages/Main';
-import Nav from './components/Nav';
-import Jumbotron from './components/Jumbotron';
-import SoundSection from './components/SoundSection';
-import DisplaySection from './components/DisplaySection';
 
 function App() {
 
   return (
     <div className='App'>
-      <Nav/>
-      <Jumbotron/>
-      <SoundSection/>
-      <DisplaySection/>
+      
     </div>
   )
 }
