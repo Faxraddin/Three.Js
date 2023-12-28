109405b55dc7c39cf3f594b9e503bb127ee80e64
Faxraddin <reydivonay@gmail.com>
2023-12-28 21:08:17 +0400

    Plane and Bird 3d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React,{Suspense} from "react";
+import React,{Suspense,useState} from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 
 import { Island } from "../models/Island";
 import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
 const Home = () => {
+    const [isRotating,setIsRotating] = useState(false);
+    
     const adjustIslandForScreenSize = () => {
       let screenScale = null;
       let screenPosition = [0, -6.5, -43];
@@ -32,13 +36,16 @@ const Home = () => {
 
               <directionalLight position={[1,1,1]} intensity={2}/>
               <ambientLight intensity={0.5}/>
+              <hemisphereLight skyColor='#b1e1ff' groundColor={'#000000'} intensity={1}/> 
               
+              <Bird/>
               <Sky/>
               <Island
                 position={islandPosition}
                 scale={islandScale}
                 rotation = {islandRotation}
               />
+              <Plane/>
 
             </Suspense>
           </Canvas>
