fb331f8dd3d13b32701f5f573f7afb1f5096bdf3
Faxraddin <reydivonay@gmail.com>
2023-12-28 18:02:26 +0400

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import React from 'react';
 import './App.css';
-import Main from './pages/Main';
+import { Route,Routes } from 'react-router-dom';
+import MainRoot from './root/MainRoot';
+
+import Home from '../src/pages/Home';
+import About from '../src/pages/About';
+import Contact from '../src/pages/Contact';
+import Projects from '../src/pages/Projects';
 
 function App() {
 
   return (
-    <div className='App'>
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<MainRoot/>}>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+
+      </Route>
+    </Routes>
   )
 }
 
